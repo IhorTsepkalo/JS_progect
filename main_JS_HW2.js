f60e361161f8aa2fd0396edf2a7a5dfb93e45b07
IhorTsepkalo <igorcepkalo@gmail.com>
2021-03-26 13:37:44 +0200

    JS_HW2 + some additional tasks.

--- main_JS_HW2.js
+++ main_JS_HW2.js
@@ -4,7 +4,7 @@
 // - з 5 значень стрічкового, числового та булевого типу
 //let words = ['january', 'february', 'march', 'april', 'may'];
 //let numbers = [1, 2, 3, 4, 5];
-//let mix = ['day', 12, true, false, 'm=night'];
+//let mix = ['day', 12, true, false, 'night'];
 //console.log(words);
 //console.log(numbers);
 //console.log(mix);
@@ -33,31 +33,31 @@
 //}
 
 // 5. - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
-//let monthes = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
-//let i = 0;
-//while (i<20) {
-//    document.write(`<div><h1>${monthes[i]}</h1></div>`);
-//i++
-//}
-
-// 6. За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-//let monthes = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
-//let i = 0;
-//while (i<20) {
-//    i++
-//    document.write(`<div><h1>${monthes[i]}</h1></div>`);
-//}
-
-// 7. Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
-// let numbers = [1,2,3,4,5,6,7,8,9,10];
-// for (let i=0;i<numbers.length;i++) {
-//     console.log(numbers[i])
+// let months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+// let i = 0;
+// while (i<20) {
+//     document.write(`<div><h1>${months[i]}</h1></div>`);
+// i++
 // }
 
+// 6. За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+// let months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september'];
+// let i = 0;
+// while (i<20) {
+//     i++
+//     document.write(`<div><h1>${months[i-1]}</h1></div>`);
+// }
+
+// 7. Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+//let numbers = [1,2,3,4,5,6,7,8,9,10];
+//for (let i=0;i<numbers.length;i++) {
+//    console.log(numbers[i])
+//}
+
 // 8. Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
-// let monthes = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october'];
-// for (let i=0;i<monthes.length;i++){
-//     console.log(monthes[i])
+// let months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october'];
+// for (let i=0;i<months.length;i++){
+//     console.log(months[i])
 // }
 
 // 9. Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
@@ -75,11 +75,11 @@
 //}
 
 // 11. Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
-// let any = ['january', 3, 'march', true, false, 'june', 45, '50', 'september', 'october'];
-// for(let i=0;i<any.length;i++) {
-//     if(typeof any[i] === 'number') {
-//         document.write(`<div>${any[i]}</div>`)
-//     }
+//let any = ['january', 3, 'march', true, false, 'june', 45, '50', 'september', 'october'];
+//for(let i=0;i<any.length;i++) {
+//    if(typeof any[i] === 'number') {
+//        document.write(`<div>${any[i]}</div>`)
+//    }
 //}
 
 // 12. Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
@@ -91,27 +91,26 @@
 //}
 
 // 13.  Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
-
-// let empty = [];
-//  empty[0] = 'january';
-//  empty[1] = 'february';
-//  empty[2] = 'march';
-//  empty[3] =4;
-//  empty[4] ='june';
-//  empty[5] ='july';
-//  empty[6] =true;
-//  empty[7] ='september';
-//  empty[8] =false;
-//  empty[9] =48;
-// for (let i=0;i<empty.length;i++) {
-//     console.log(empty[i])
-// }
+//let empty = [];
+// empty[0] = 'january';
+// empty[1] = 'february';
+// empty[2] = 'march';
+// empty[3] =4;
+// empty[4] ='june';
+// empty[5] ='july';
+// empty[6] =true;
+// empty[7] ='september';
+// empty[8] =false;
+// empty[9] =48;
+//for (let i=0;i<empty.length;i++) {
+//    console.log(empty[i])
+//}
 
 // 14. Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-// for (let i=0;i<10;i++) {
-//     console.log(i+1)
-//     document.write(`<div>${i+1}</div>`)
-// }
+//for (let i=0;i<10;i++) {
+//    console.log(i)
+//    document.write(`<div>${i}</div>`)
+//}
 
 // 15. Створити цикл for на 100  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
 // for (let i=0;i<100;i++) {
@@ -121,21 +120,21 @@
 
 // 16. Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
 // for (let i=0;i<100;i+=2) {
-//     console.log(i+1);
-//     document.write(`<div>${i+1}</div>`);
+    // console.log(i);
+    // document.write(`<div>${i}</div>`);
 // }
 // 17.  Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
-// for (let i=0;i<100;i++) {
-//     i++;
-//     console.log(i+1);
-//     document.write(`<div>${i+1}</div>`);
+// for (let i=0;i<=100;i+=2) {
+//     console.log(i);
+//     document.write(`<div>${i}</div>`);
 // }
 
 // 18. Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
-// for (let i=0;i<100;i+=2) {
-//     console.log(i+1);
-//     document.write(`<div>${i+1}</div>`);
-// }
+//for (let i=0;i<100;i++) {
+//    i++
+//    console.log(i);
+//    document.write(`<div>${i}</div>`);
+//}
 
 // 19. Відтворити роботу годинника, відрахувавши 2 хвилини (2 цикли! 1й - хвилини, 2й - секунди)
 //for (let m = 1;m<=2;m++) {
@@ -154,4 +153,26 @@
 //         document.write(`<p><h2>${m}</h2></p>`)
 //     }
 //     document.write(`<div style="color:red"><h1>${h}</h1></div>`)
+// }
+
+/////////////// Додатково
+
+// Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+// let word = ['a', 'b', 'c'];
+// for (i=0;i<word.length;i++) {
+//     document.write(`${word[i]}`)
+// }
+
+// Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+//let word = ['a', 'b', 'c'];
+//let i = 0;
+//while (i<word.length) {
+//    i++
+//    document.write(`${word[i-1]}`);
+//}
+
+// Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+// let letters = ['a', 'b', 'c'];
+// for (let createdWord of letters) {
+//     document.write(`${createdWord}`)
 // }
