a547c1a472a67e6d88bbb27434c3df9dae81837e
IhorTsepkalo <igorcepkalo@gmail.com>
2021-03-27 14:12:22 +0200

    one branche

---
+++ JS_HW1/main_JS_HW1.js
@@ -0,0 +1,439 @@
+// 1 task. присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+//let a;
+//a1 = 'hello';
+//a2 = 'owu';
+//a3 = 'com';
+//a4 = 'ua';
+//a5 = 1;
+//a6 = 10;
+//a7 = -999;
+//a8 = 123;
+//a9 = 3.14;
+//a10 = 2.7;
+//a11 = 16;
+//a12 = true;
+//a13 = false;
+//
+//console.log(a1);
+//console.log(a2);
+//console.log(a3);
+//console.log(a4);
+//console.log(a5);
+//console.log(a6);
+//console.log(a7);
+//console.log(a8);
+//console.log(a9);
+//console.log(a10);
+//console.log(a11);
+//console.log(a12);
+//console.log(a13);
+//
+//document.write(a1+' '+a2+' '+a3+' '+a4+' '+a5+' '+a6+' '+a7+a8+' '+a9+' '+a10+' '+a11+' '+a12+' '+a13)
+//
+//alert(a1)
+//alert(a2);
+//alert(a3);
+//alert(a4);
+//alert(a5);
+//alert(a6);
+//alert(a7);
+//alert(a8);
+//alert(a9);
+//alert(a10);
+//alert(a11);
+//alert(a12);
+//alert(a13);
+
+// 2 task. переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+//let a;
+//a1 = 'no';
+//a2 = 'my';
+//a3 = 7
+//a4 = 'go';
+//a5 = 'home';
+//a6 = 45;
+//a7 = 'lady';
+//a8 = -5;
+//a9 = 'look';
+//a10 = 3.3;
+//a11 = '55 days';
+//a12 = true;
+//a13 = false;
+//
+//console.log(a1);
+//console.log(a2);
+//console.log(a3);
+//console.log(a4);
+//console.log(a5);
+//console.log(a6);
+//console.log(a7);
+//console.log(a8);
+//console.log(a9);
+//console.log(a10);
+//console.log(a11);
+//console.log(a12);
+//console.log(a13);
+//
+//document.write(a1 + ' ' + a2 + ' ' + a3 + ' ' + a4 + ' ' + a5 + ' ' + a6 + ' ' + a7 + a8 + ' ' + a9 + ' ' + a10 + ' ' + a11 + ' ' + a12 + ' ' + a13)
+//
+//alert(a1)
+//alert(a2);
+//alert(a3);
+//alert(a4);
+//alert(a5);
+//alert(a6);
+//alert(a7);
+//alert(a8);
+//alert(a9);
+//alert(a10);
+//alert(a11);
+//alert(a12);
+//alert(a13);
+
+// 3 task. Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+//const age = 33;
+//const heigh = 1.76;
+//const weigh = 76;
+//const firstName = 'Ihor';
+//const secondName = 'Tsepkalo';
+//const countryOrigin = 'Ukraine';
+//
+//console.log(age);
+//console.log(firstName);
+//console.log(secondName);
+//console.log(countryOrigin);
+//console.log(heigh);
+//console.log(weigh);
+//
+//alert(age);
+//alert(firstName);
+//alert(secondName);
+//alert(countryOrigin);
+//alert(heigh);
+//alert(weigh);
+
+// 4 task. при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+//let firstName = prompt('First Name');
+//console.log(firstName);
+//let secondName = prompt('Second Name');
+//console.log(secondName);
+//let countryOrigin = prompt('Country of Origin');
+//console.log(countryOrigin);
+//
+//alert(firstName);
+//alert(secondName);
+//alert(countryOrigin);
+//
+//document.write(firstName+' '+secondName+' '+countryOrigin);
+
+// 5 task. Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+//let firstName = 'Ihor';
+//let secondName = 'Tsepkalo';
+//let countryOrigin = 'Ukraine';
+//let person = (firstName+' '+secondName+' '+countryOrigin);
+//console.log(person);
+
+// 6 task. Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+//let firstNameP1 = prompt('First Name person 1');
+//console.log(firstNameP1);
+//let secondNameP1 = prompt('Second Name person 1');
+//console.log(secondNameP1);
+//let countryOriginP1 = prompt('Country of Origin person 1');
+//console.log(countryOriginP1);
+//alert(firstNameP1);
+//alert(secondNameP1);
+//alert(countryOriginP1);
+//let person1 = (firstNameP1+' '+secondNameP1+' '+countryOriginP1);
+//console.log(person1);
+//document.write(firstNameP1+' '+secondNameP1+' '+countryOriginP1);
+//
+//let firstNameP2 = prompt('First Name person 2');
+//console.log(firstNameP2);
+//let secondNameP2 = prompt('Second Name person 2');
+//console.log(secondNameP2);
+//let countryOriginP2 = prompt('Country of Origin person 2');
+//console.log(countryOriginP2);
+//alert(firstNameP2);
+//alert(secondNameP2);
+//alert(countryOriginP2);
+//let person2 = (firstNameP2+' '+secondNameP2+' '+countryOriginP2);
+//console.log(person2);
+//document.write(firstNameP2+' '+secondNameP2+' '+countryOriginP2);
+//
+//let firstNameP3 = prompt('First Name person 3');
+//console.log(firstNameP3);
+//let secondNameP3 = prompt('Second Name person 3');
+//console.log(secondNameP3);
+//let countryOriginP3 = prompt('Country of Origin person 3');
+//console.log(countryOriginP3);
+//alert(firstNameP3);
+//alert(secondNameP3);
+//alert(countryOriginP3);
+//let person3 = (firstNameP3+' '+secondNameP3+' '+countryOriginP3);
+//console.log(person2);
+//document.write(firstNameP3+' '+secondNameP3+' '+countryOriginP3);
+
+// 7 task. при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+//let age1 = +prompt('your age');
+//console.log(age1);
+//let heigh = +prompt('your heigh');
+//console.log(heigh);
+//let weight = +prompt('your weight');
+//console.log(weight);
+
+// 8 task.  при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+//let weight1 = prompt('weight1');
+//weight1 = parseInt(weight1);
+//let weight2 = prompt('weight2');
+//weight2 = parseInt(weight2);
+//let weight3 = prompt('weight3');
+//weight3 = parseInt(weight3);
+//let result = (weight1+weight2+weight3);
+//console.log(result);
+
+// 9 task. при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+//let weight1 = prompt('weight1');
+//weight1 = parseFloat(weight1);
+//let weight2 = prompt('weight2');
+//weight2 = parseFloat(weight2);
+//let weight3 = prompt('weight3');
+//weight3 = parseFloat(weight3);
+//let result = (weight1+weight2+weight3);
+//console.log(result);
+
+// 10 task. при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+//let weight1 = prompt('weight1');
+//weight1 = parseFloat(weight1);
+//weight1 = Math.round(weight1);
+//let weight2 = prompt('weight2');
+//weight2 = parseFloat(weight2);
+//weight2 = Math.round(weight2);
+//let weight3 = prompt('weight3');
+//weight3 = parseFloat(weight3);
+//weight3 = Math.round(weight3);
+//let result = (weight1+weight2+weight3);
+//console.log(result);
+
+// 11 task. при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+//let mainNum = prompt('Main Number');
+//mainNum = parseFloat(mainNum);
+//mainNum = Math.round(mainNum);
+//let exponent = prompt('Exponent');
+//exponent = parseFloat(exponent);
+//exponent = Math.round(exponent);
+//let result = Math.pow(mainNum,exponent);
+//console.log(result);
+
+// 12 task. При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+//let a = 100;
+//console.log(typeof a);
+//let b = '100';
+//console.log(typeof b);
+//let c = true;
+//console.log(typeof c);
+//let d = undefined;
+//console.log(typeof d);
+
+// 12 task.  Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат. В одинаковых выражениях в задании не использовать два одинаковых оператора. Каждое выражение вложить в свою переменную
+//let a0 = 5 < 6;
+//console.log(a0);
+//let a1 = 5 > 6;
+//console.log(a1);
+//let a2 = 5 === 6;
+//console.log(a2);
+//let a3 = 5 >= 6;
+//console.log(a3);
+//let a4 = 10 === 10;
+//console.log(a4);
+//let a5 = 10 >= 10;
+//console.log(a5);
+//let a6 = 10 > 10;
+//console.log(a6);
+//let a7 = 10 < 10;
+//console.log(a7);
+//let a8 = 10 < 10;
+//console.log(a8);
+//let a9 = 123 === '123';
+//console.log(a9);
+//let a10 = 123 == '123';
+//console.log(a10);
+
+// 13 task. Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+//console.log(132 > 100 && 45 < 12 ); false
+//console.log(34 > 33 && 23 < 90 ); true
+//console.log(99 > 100 && 45 > 12 ); false
+//console.log(132 > 100 || 45 < 12 ); true
+//console.log(111 > 11 || 45 < 111 ); true
+//console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+//console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); true
+//console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+//console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+//console.log(!!'-1'); true
+//console.log(!!-1); true
+//console.log(!!'0'); true
+//console.log(!!'null'); true
+//console.log(!!'undefined'); true
+//console.log(!!(3/'owu')); false
+//console.log((111 > 11 || 45 < 111) ||  !!'0'); true
+//console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); false
+
+
+///////////////////CLASS
+
+// 1 task. Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true». При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.
+//let str = '"Привіт"';
+//console.log(typeof str);
+//let num = 123;
+//console.log(typeof num);
+//let flag = true;
+//console.log(typeof flag);
+//let txt = '"true"';
+//console.log(typeof txt);
+
+// 2 task. Создайте переменные a1, a2, a3, a4, a5. При помощи математических операторов (сложение, вычитание и т.д.) найдите значения выражений. поместив результат каждого выражения в соответствующую переменную. Например, let a1 = 5 + 3.
+//let a1 = 5 + 3;
+//console.log(a1);
+//let a2 = 5 - 3;
+//console.log(a2);
+//let a3 = 5 * 3;
+//console.log(a3);
+//let a4 = 5 / 3;
+//console.log(a4);
+//let a5 = 5 % 3;
+//console.log(a5);
+
+// 3 task. Создайте переменные a6, a7, a8, a9, a10. Поместите в них результат выражений:
+//let a6 = 5 % 3;
+//console.log(a6);
+//let a7 = 3 % 5;
+//console.log(a7);
+//let a8 = 5 + '3';
+//console.log(a8);
+//let a9 = '5' - 3;
+//console.log(a9);
+//let a10 = 75 + 'кг';
+//console.log(a10);
+
+// 4 task. Напишите код, который находит площадь прямоугольника высота 23см. ( переменная height), шириной 10см (переменная width), значение площади должно хранится в числовой переменной s.
+//let height = 23;
+//let width = 10;
+//let s = width*height;
+//console.log(s);
+
+// 5 task. Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC), результат поместите в переменную v.
+//let height = 10;
+//let dC = 4;
+//let p = 3.14;
+//let v = p*Math.pow(dC/2, 2)*height;
+//console.log(v);
+
+// 6 task. У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4). Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор возведения в степень ** ).
+//let n = 3;
+//let m = 4;
+//let k = Math.sqrt((Math.pow(n,2) + Math.pow(m,2)));
+//console.log(k);
+
+// 7 task. Напишите скрипт, который выводит "Hello world", создавши переменную str и выводить спомощью document.write, alert и console.log
+//let str = 'Hello World';
+//document.write(str);
+//alert(str);
+//console.log(str);
+
+// 8 task. Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби (каждой на новой строки спомощью \n).
+//let name = 'Ігор Васильович Цепкало';
+//let age = '33 роки';
+//let hobby = 'футбол';
+//alert(name);
+//alert(age);
+//alert(hobby);
+//document.write(`<p>${name}</p> <p>${age}</p> <p>${hobby}</p>`);
+
+// 9 task. Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
+// Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’ Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
+// Вывести в документ содержимое переменной concatenation спомощью document.write
+//let str1 = 'Хто';
+//let str2 = 'ти';
+//let str3 = 'такий?';
+//let concatenation = (str1+' '+str2+' '+str3);
+//document.write(concatenation);
+
+// 10 task. Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//let str = "20";
+//let a = 5;
+//document.write(str + a+ "<br/>");//205
+//document.write(str - a + "<br/>");//15
+//document.write(str * "2" + "<br/>");//40
+//document.write(str / 2 + "<br/>");//10
+
+// 11 task. Какие значения выведет в окно консоли следующий фрагмент кода если его поместить в console.log?
+//parseInt("3.14")//3
+//parseInt("-7.875")//-7
+//parseInt("435")//435
+//parseInt("Вася")//NoN
+
+// 12 task.  С помощью окна ввода, вызываемого методом prompt, пользователь может ввести данные, которые будут использоваться далее, повторите код ниже
+//let str = prompt("Enter something", "ho-ho");
+//console.log(str);
+
+// 13 task.  С помощью окна ввода, вызываемого методом prompt, сделать сложение двух чисел, а вывод результата при помощи метода alert
+//let a1 = +prompt('number 1');
+//let b2 = +prompt('number 2');
+//let summ = a1 +  b2;
+//alert(summ);
+
+// 14 task. С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию и возраст, а вам не обходимо вывести строку такого вида
+//       Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert
+//let firstName = prompt('Імя');
+//let secondName = prompt('Прізвище');
+//let age = +prompt('Вік');
+//let welcome = 'Добрий вечір' + ' ' + firstName + ' ' + secondName + ' ' + 'мої вітання, що вам' + ' ' +age;
+//alert(welcome);
+
+//////////////////////////////Додатково
+
+// 1 task. Три різних числа вводяться через prompt().
+// За допомоги if else вивести іх в порядку зростання. (відсортувати по зростанню)
+//let a = +prompt('Число 1');
+//let b = +prompt('Число 2');
+//let c = +prompt('Число 3');
+//
+//if (a > b && a > c && b > c) {
+//    console.log(a);
+//    console.log(b);
+//    console.log(c)
+//} else if (b > a && b > c && a > c) {
+//    console.log(b);
+//    console.log(a);
+//    console.log(c);
+//} else if (c > b && c > a && a > b) {
+//    console.log(c);
+//    console.log(a);
+//    console.log(b);
+//} else if (a > b && c > b && a > c) {
+//    console.log(a);
+//    console.log(c);
+//    console.log(b);
+//} else if (b > a && b > c && c > a) {
+//    console.log(b);
+//    console.log(c);
+//    console.log(a);
+//} else if (c > b && c > a && b > a) {
+//    console.log(c);
+//    console.log(b);
+//    console.log(a);
+//}
+
+// 2 task.
+//let color = prompt('Enter color');
+//if (color === 'green') {
+//console.log('go')
+//}
+//else if (color === 'yellow') {
+//    console.log('get ready')
+//}
+//else if (color === 'red') {
+//    console.log('wait')
+//}
+//else {
+//    console.log('what a fuk')
+//}
